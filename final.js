3df8f6bfa13482ac4027cadee1793633a547aec6
/Di <mybeatastultica@gmail.com>
2025-07-22 13:53:02 +0300

    Add files via upload

---
+++ final.js
@@ -0,0 +1,33 @@
+const bigImage = document.querySelector("#big-image")
+
+const img1 = document.querySelector("#img-1")
+const img2 = document.querySelector("#img-2")
+const img3 = document.querySelector("#img-3")
+const img4 = document.querySelector("#img-4")
+const img5 = document.querySelector("#img-5")
+const img6 = document.querySelector("#img-6")
+const img7 = document.querySelector("#img-7")
+
+
+img1.addEventListener("click", () => {
+    bigImage.src = img1.src
+})
+img2.addEventListener("click", () => {
+    bigImage.src = img2.src
+})
+img3.addEventListener("click", () => {
+    bigImage.src = img3.src
+})
+img4.addEventListener("click", () => {
+    bigImage.src = img4.src
+})
+img5.addEventListener("click", () => {
+    bigImage.src = img5.src
+})
+img6.addEventListener("click", () => {
+    bigImage.src = img6.src
+})
+img7.addEventListener("click", () => {
+    bigImage.src = img7.src
+})
+
